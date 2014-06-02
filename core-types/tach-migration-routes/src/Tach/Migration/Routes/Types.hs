fe53df02b49ccc73e6544f90db63ed36242f39c6
Kevin Cotrone <kevincotrone@gmail.com>
2014-06-01 23:08:28 -0500

    Forgotten file

---
+++ core-types/tach-migration-routes/src/Tach/Migration/Routes/Types.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeFamilies, RecordWildCards, OverloadedStrings, DeriveGeneric, GeneralizedNewtypeDeriving #-}
+module Tach.Migration.Routes.Types where
+
+--General Haskell imports
+import Control.Concurrent.STM.TMVar
+import Control.Concurrent.MVar
+import GHC.Generics
+import qualified Data.ByteString as BS
+import qualified Network.AWS.S3Simple as S3
+import Data.Text
+import qualified Data.Text.Encoding as TE
+import Data.Serialize
+import Data.SafeCopy        ( SafeCopy, base, deriveSafeCopy )
+
+-- Acid and file related
+import Data.Acid
+
+-- Containers
+import qualified Data.Set as S
+import qualified Data.Map as M
+import qualified Data.Vector as V
+
+--External Tach imports
+import Tach.Impulse.Types.TimeValue
+import Tach.Impulse.Types.Impulse
+import Tach.Migration.Acidic.Types
+import Tach.Periodic
+import Tach.Transformable.Types
+
+-- Yesod and web related
+import Yesod
+import Yesod.Core
+import Yesod.Core.Types
+
+-- Used for serializing and deserializing keys for indexing
+import qualified DirectedKeys as DK
+import qualified DirectedKeys.Types as DK
+import qualified Data.Serialize as S
+
+--Directly related Tach imports
+import Tach.Migration.Routes.Internal
+
+--Wavelets and Compression
+import qualified Codec.Compression.GZip as GZ
+import Data.Wavelets.Construction
+import Tach.Migration.Types
+
+import Data.Acid.Cell
+
+data MigrationRoutes = MigrationRoutes {
+ migrationRoutesAcidCell :: MigrationCell
+ ,migrationRoutesTVKeySet :: S.Set TVKey                             --A set of TVKeys to handle which PIDs it is responsible for
+ ,s3Conn :: !S3.S3Connection
+ ,stateMap :: TMVar (M.Map IncomingKey StateStatus)
+ ,migrationRoutesDestination :: !String
+ ,migrationRoutesWait :: MVar Int
+}
+
+data StateStatus = Uploading | Idle
+
+type MigrationCell = AcidCell KeyPid KeySource KeyDestination KeyTime TVSimpleImpulseTypeStore (AcidState CellKeyStore)
+
+buildKeyImpulseStore :: (DK.DirectedKeyRaw KeyPid KeySource KeyDestination KeyTime) -> TVSimpleImpulseTypeStore
+buildKeyImpulseStore key = TVSimpleImpulseTypeStore (ImpulseSeries (ImpulseKey key) ( IPeriodConst 0 ) (ImpulseStart 0) (ImpulseEnd 0) (ImpulseRep S.empty))
+
+mkYesodData "MigrationRoutes" $(parseRoutesFile "migration-routes")
