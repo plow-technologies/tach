ce001febab239e9c365f04db16ac3da8906fed6c
Kevin Cotrone <kevincotrone@gmail.com>
2014-06-26 11:07:14 -0500

    Adding applicative and monad instances for Classify

--- core-types/tach-typeclasses/src/Tach/Types/Classify.hs
+++ core-types/tach-typeclasses/src/Tach/Types/Classify.hs
@@ -3,13 +3,24 @@
 module Tach.Types.Classify where
 
 import           Data.Bifunctor
+import Control.Applicative
 
-data Classify a b = Unclassified a | Classified b deriving (Show, Eq, Ord)
+data Classify a b = Classified a | Unclassified b deriving (Show, Eq, Ord)
 
 instance Bifunctor Classify where
-  first f (Unclassified u) = Unclassified (f u)
-  second g (Classified c) = Classified (g c)
+  first f (Classified u) = Classified (f u)
+  second g (Unclassified c) = Unclassified (g c)
 
 instance Functor (Classify a) where
-  fmap _ (Unclassified u) = Unclassified u
-  fmap f (Classified c) = Classified (f c)
+  fmap _ (Classified u) = Classified u
+  fmap f (Unclassified c) = Unclassified (f c)
+
+instance Applicative (Classify a) where
+  pure = Unclassified
+  Classified e <*> _ = Classified e
+  Unclassified f <*> u = fmap f u
+
+instance Monad (Classify a) where
+  return = Unclassified
+  Classified c >>= _ = Classified c
+  Unclassified un >>= f = f un
