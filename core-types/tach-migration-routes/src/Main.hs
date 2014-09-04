aed25d90c63831434d67bc145a61694d5c3dc08e
Daniel-Diaz <dhelta.diaz@gmail.com>
2014-09-04 17:54:20 +0200

    Refactor of main module. Added type signatures. Removed unused imports.

--- core-types/tach-migration-routes/src/Main.hs
+++ core-types/tach-migration-routes/src/Main.hs
@@ -1,36 +1,25 @@
+
 {-# LANGUAGE DeriveGeneric, DeriveDataTypeable, RecordWildCards, OverloadedStrings #-}
+
 module Main where
 
 import GHC.Generics
 import Control.Applicative
-import System.Console.CmdArgs
+import System.Console.CmdArgs hiding (args)
 import Control.Monad
-import Network.AWS.S3Simple
 import Control.Concurrent.STM
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as C
-import qualified Data.Either as E
 import qualified Data.Yaml as Y
 import qualified Data.Text as T
 import Yesod.Core
-import Yesod
 import qualified Data.Set as S
 import qualified Data.Map as M
-import Tach.Migration.Routes
-import Tach.Migration.Types
-import qualified DirectedKeys as DK
-import qualified DirectedKeys.Types as DK
 import Network.Wai.Handler.Warp
 import qualified Filesystem.Path.CurrentOS as OS
 
 -- Acid and file related
 import Data.Acid
-import Data.Acid.Advanced
-import Data.Acid.Local
 import Control.Concurrent
-import Control.Concurrent.STM.TMVar
-import Control.Concurrent.STM.TVar
-import Control.Concurrent.MVar
 import Tach.Migration.Foundation
 import Tach.Migration.Routes.Types
 import Control.Exception
@@ -38,9 +27,9 @@ import Data.Yaml
 import Data.Streaming.Network.Internal
 
 import qualified Filesystem as FS
-import qualified Filesystem.Path as FP
 
-import Tach.Impulse.Types.TimeValue
+-- Instances
+import Tach.Migration.Routes()
 
 data MigrationPath = MigrationPath{ 
    migrationPath :: String
@@ -53,53 +42,53 @@ data MigrationPath = MigrationPath{
 main :: IO ()
 main = do
   args <- cmdArgs $ MigrationPath "config.yml"
-  let fullPath = OS.fromText $ T.pack (migrationPath args)
-  putStrLn $ show args
+  let fullPath = OS.fromText $ T.pack $ migrationPath args
+  print args
   file <- FS.readFile fullPath
-  let eConf =  Y.decodeEither file
+  let eConf = Y.decodeEither file
   case eConf of
-    (Left _) -> putStrLn "Error reading config file"
-    (Right conf) -> do
+    Left _ -> putStrLn "Error reading config file"
+    Right theConf -> do
       migrationConfig <- readMigrationConfig fullPath
-      runServer conf migrationConfig
-      where runServer conf migrationConf = do
-              let dKey = buildIncomingKey (KeyPid 300) (KeySource "www.aacs-us.com") (KeyDestination "http://cloud.aacs-us.com") (KeyTime 0)
-              cells <- initializeTVSimpleImpulseTypeStoreAC (migrationStatePath migrationConf)
-              resMap <- foldlWithKeyTVSimpleImpulseTypeStoreAC cells (\_ key _ ioStates -> (M.insert key Idle) <$> ioStates) (return M.empty)
-              sMap <- newTMVarIO resMap
-              wait <- newEmptyMVar
-              gcState <- newTVarIO GCIdle
-              forkIO . void $ gcRunner ((migrationGCDelayMinutes migrationConf) * 60 * 1000 * 1000) gcState
-              forkIO $ notWarpWarp migrationConf (MigrationRoutes cells S.empty conf sMap "http://cloud.aacs-us.com" wait (migrationS3Bucket migrationConf) (migrationStatePath migrationConf) gcState) wait
-              res <- takeMVar wait
-              return ()
-              where impulseStateMap state key = M.singleton key state
+      let runServer conf migrationConf = do
+            let -- dKey = buildIncomingKey (KeyPid 300) (KeySource "www.aacs-us.com") (KeyDestination "http://cloud.aacs-us.com") (KeyTime 0)
+                -- impulseStateMap = flip M.singleton
+            cells <- initializeTVSimpleImpulseTypeStoreAC (migrationStatePath migrationConf)
+            resMap <- foldlWithKeyTVSimpleImpulseTypeStoreAC cells (\_ key _ ioStates -> M.insert key Idle <$> ioStates) (return M.empty)
+            sMap <- newTMVarIO resMap
+            wait <- newEmptyMVar
+            gcState <- newTVarIO GCIdle
+            _ <- forkIO . void $ gcRunner (migrationGCDelayMinutes migrationConf * 60 * 1000 * 1000) gcState
+            _ <- forkIO $ notWarpWarp migrationConf
+                                      (MigrationRoutes cells S.empty conf sMap "http://cloud.aacs-us.com" wait (migrationS3Bucket migrationConf) (migrationStatePath migrationConf) gcState)
+                                      wait
+            void $ takeMVar wait
+      runServer theConf migrationConfig
 
 -- createWarp port cells keys conn sMap host wait = do
 --   warp 3000 (MigrationRoutes cells (S.singleton . buildTestImpulseKey $ dKey) conn sMap "http://cloud.aacs-us.com" wait)
 
+notWarpWarp :: MigrationConfig -> MigrationRoutes -> MVar a -> IO ()
 notWarpWarp config app wait = do
-  finally (do
-            putStrLn ("Starting migration server" :: String)
-            app <- toWaiApp app
-            void $ startServer app config 
-            res <- takeMVar wait
-            _ <- putMVar wait res
-            return ())
+  finally (do putStrLn $ "Starting migration server"
+              wapp <- toWaiApp app
+              _ <- startServer wapp config 
+              res <- takeMVar wait
+              _ <- putMVar wait res
+              return ()
+           )
           (void $ do
-            putStrLn ("Closing migration server" :: String)
-            let cells = (migrationRoutesAcidCell app)
-            void $ do
-              void $ archiveAndHandleTVSimpleImpulseTypeStoreAC cells (\ _ b -> do
-                                                                            createCheckpoint b
-                                                                            return b)
-              createCheckpointAndCloseTVSimpleImpulseTypeStoreAC cells
+              putStrLn ("Closing migration server" :: String)
+              let cells = migrationRoutesAcidCell app
+              void $ do
+                _ <- archiveAndHandleTVSimpleImpulseTypeStoreAC cells (\_ b -> createCheckpoint b >> return b)
+                createCheckpointAndCloseTVSimpleImpulseTypeStoreAC cells
             )
 
-startServer app conf = do
-  let warpSettings = (setFdCacheDuration 0) . (setTimeout 120) . (setHost (Host $ T.unpack (migrationHost conf))) . (setPort . migrationPort $ conf) $ defaultSettings
-  runSettings warpSettings app
-
+startServer :: Application -> MigrationConfig -> IO ()
+startServer app conf = runSettings warpSettings app
+  where
+    warpSettings = setFdCacheDuration 0 . setTimeout 120 . setHost (Host $ T.unpack $ migrationHost conf) . (setPort . migrationPort $ conf) $ defaultSettings
 
 data MigrationConfig = MigrationConfig { 
       migrationPort :: Int 
@@ -111,8 +100,8 @@ data MigrationConfig = MigrationConfig {
 
 readMigrationConfig :: OS.FilePath -> IO MigrationConfig
 readMigrationConfig fPath = do
-  fCont <- BS.readFile (OS.encodeString fPath)
-  either (\e -> fail e) (\asc -> return asc) $ decodeEither $ fCont
+  fCont <- BS.readFile $ OS.encodeString fPath
+  either fail return $ decodeEither $ fCont
 
 instance FromJSON MigrationConfig where
 instance ToJSON MigrationConfig where
@@ -124,11 +113,11 @@ gcRunner delay tv = forever $ do
   atomically $ writeTVar tv GCStart
   waitTVar tv
 
-
+waitTVar :: TVar GCState -> IO ()
 waitTVar tv = do
   status <- atomically $ readTVar tv
   case status of
     GCIdle -> return ()
-    otherwise -> do
+    _ -> do
       threadDelay 10000000 -- 10 seconds
       waitTVar tv
