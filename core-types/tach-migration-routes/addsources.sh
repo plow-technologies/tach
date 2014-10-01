091f762d004a9645aadbdd085a3c4afa2f749ef2
Daniel Diaz <dhelta.diaz@gmail.com>
2014-10-01 23:52:56 +0200

    Provisional addsources.sh. This won't be needed once the build is fully fixed and dependencies uploaded to our Hackage.

---
+++ core-types/tach-migration-routes/addsources.sh
@@ -0,0 +1,7 @@
+cabal sandbox add-source ../tach-impulse-series-types
+cabal sandbox add-source ../tach-migration-types
+cabal sandbox add-source ../tach-migration-acidic
+cabal sandbox add-source ../tach-transformable-types
+cabal sandbox add-source ../../core-libs/tach-acid-impulse-lib
+cabal sandbox add-source ../../binary-store
+cabal sandbox add-source ../../binary-transform
